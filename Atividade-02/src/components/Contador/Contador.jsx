5eb2675664539f0093803aa089c4b39134eb6ea6
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-10-31 17:58:45 -0300

    Exercicio 02

--- Atividade-02/src/components/Contador/Contador.jsx
+++ Atividade-02/src/components/Contador/Contador.jsx
@@ -4,26 +4,55 @@ import './Contador.css'
 function Contador () {
 
   const [contador, setContador ] = useState(0)
-  const [mls, setMls ] = useState()
+  const [mls, setMls ] = useState(300)
+  const [total, setTotal ] = useState(0)
+  const [metaDiaria, setMetaDiaria ] = useState(1000)
+  const [mensagem, setMensagem ] = useState()
+  const [horario, setHorario] = useState()
 
-  function somar () {
-    setContador(prevState => prevState + 1)
+  function somar () { 
+    if ((mls > 0)) {
+      if ((metaDiaria > 0)) {
+        setContador(prevState => prevState + 1)
+        setMls(prevState => prevState)
+        setTotal(prevState => prevState + mls)
+      } else{
+        setMensagem("A quantidade de ML deve ser maior que 0")
+      }
+
+      if (total >= metaDiaria) {
+        setMensagem("Parabéns, você atingiu sua meta diária de consumo")
+      }
+      
+      setHorario(new Date().toLocaleTimeString());
+    } else {
+      setMensagem("A quantidade de ML deve ser maior que 0")
+    }
+
   }
 
   function zerar () {
     setContador(0)
-    setMls('')
+    setMls(0)
+    setTotal(0)
+    setMensagem()
   }
 
     return (
         <div>
+            <h1></h1>
             <h1>Copos: {contador}</h1>
-            <h1>Total: 0</h1>
-            <button onClick={somar}>Somar</button>
-            <button onClick={zerar}>Zerar</button>
-            <input type="text" value={mls} onChange={e => setMls(e.target.value)} placeholder='Mls de Agua'/>
-            
-
+            <h1>Total de água consumida: {total} ML</h1>
+            <p>Medida do compo em ML:</p>
+            <input type="number" min={0} value={mls} onChange={e => setMls(parseInt(e.target.value))} placeholder='Medida'/>
+            <p>Meta diária em ML: </p>
+            <input type="number" min={0} value={metaDiaria} onChange={e => setMetaDiaria(parseInt(e.target.value))} placeholder='Meta diária'/>
+            <div>
+              <button onClick={somar}>Somar</button>
+              <button onClick={zerar}>Zerar</button>
+            </div>
+            <h2>{mensagem}</h2>
+            <h2>{horario}</h2>
         </div>
         
     )
