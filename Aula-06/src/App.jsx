3453ea8c8505897f3bd606a1e0ab46dc1ac0ba56
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-11-03 21:08:01 -0300

    Aula -06

--- Aula-06/src/App.jsx
+++ Aula-06/src/App.jsx
@@ -1,41 +1,10 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import api from './services/api'
+import Gatinho from './components/Gatinho/Gatinho'
 
 function App() {
-  const [cat, setCat] = useState(0)
-
-  //Só é executado após o componente ser renderizado
-  useEffect(() => {
-    getCat();
-  }, [])
-
-  //Fecth api faz requisições nativas
-  //Vamos usar o axios para fazer as requisições - Instalar
-
-  const getCat = async () => {
-    const { data } = await api.get("/images/search")
-    setCat(data)
-  }
-
   return (
     <>
-      <main>
-        <h1>Use Effect com Cats</h1>
-        {cat.length > 0 ? (
-          <>
-            <img src={cat[0].url} alt="Gatinho" />
-            <p>{cat.length > 0 && cat[0].url}</p>
-            <a href={cat[0].url}>Link</a>
-          </>
-        ) : (
-          <h1>Sem Imagem no momento</h1>
-        )}
-        <div>
-          <button onClick={() => getCat()}>Trocar a imagem</button>
-        </div>
-      </main>
+      <Gatinho />
     </>
   )
 }
---
+++ Aula-06/src/components/Gatinho/Gatinho.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+function Gatinho() {
+    const [cat, setCat] = useState(0)
+
+    
+    //Só é executado após o componente ser renderizado
+    useEffect(() => {
+        getCat();
+    }, [])
+
+
+    //Fecth api faz requisições nativas
+    //Vamos usar o axios para fazer as requisições - Instalar
+    const getCat = async () => {
+        const { data } = await api.get("/images/search")
+        setCat(data)
+    }
+
+    return (
+        <main>
+            <h1>Use effect with cats</h1>
+            {cat.length > 0 ? (
+                <>
+                    <img src={cat[0].url} alt="gatinho" />
+                    <p>{cat.length > 0 && cat[0].url}</p>
+                    <a href={cat[0].url}>Link</a>
+                </>
+            ) : (
+                <h1>Sem Imagem no momento</h1>
+            )}
+            <div>
+                <button onClick={() => getCat()}>Trocar a imagem</button>
+            </div>
+        </main>
+    )
+}
+
+export default Gatinho
